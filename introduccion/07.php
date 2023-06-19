98a8d8bfac932bc196b9aefb7790b820b4cd18e8
Cartago <cartago.relaxingcup@gmail.com>
2023-06-19 18:30:18 +0200

    Creamos los primeros metodos de una clase de php

--- introduccion/07.php
+++ introduccion/07.php
@@ -1,2 +1,55 @@
 <?php
 include 'includes/header.php';
+
+
+class Empleado
+{
+    public string $nombre;
+    public string $apellido;
+    public string $departamento;
+    public string $email;
+    public int $codigo;
+
+    public function __construct($nombre, $apellido, $departamento, $email, $codigo)
+    {
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->departamento = $departamento;
+        $this->email = $email;
+        $this->codigo = $codigo;
+    }
+
+    public function nombreCompleto(): string
+    {
+        return $this->nombre . " " . $this->apellido;
+    }
+}
+
+$juan = new Empleado(
+    "Juan",
+    "Perez",
+    "Ventas",
+    "juanperez@gmail.com",
+    1234
+);
+
+$karen = new Empleado(
+    "Karen",
+    "Gonzalez",
+    "Marketing",
+    "karen@gmail.com",
+    4321
+);
+
+echo $juan->nombreCompleto();
+echo $karen->nombreCompleto();
+
+
+
+echo "<pre>";
+var_dump($juan);
+echo "</pre>";
+
+echo "<pre>";
+var_dump($karen);
+echo "</pre>";
