cf420c40df59320e5c939e5d9cdea358c9a37f01
Cartago <cartago.relaxingcup@gmail.com>
2023-06-19 18:27:09 +0200

    Aprendemos que son los constructor de property promotion

--- introduccion/06.php
+++ introduccion/06.php
@@ -1,2 +1,26 @@
 <?php
 include 'includes/header.php';
+
+class Empleado
+{
+    // Contructor property Promotion
+    public function __construct(
+        public $nombre,
+        public $apellido,
+        public $departamento,
+        public $email,
+        public $codigo
+    ) {
+    }
+}
+
+$empleado = new Empleado("Juan", "Perez", "Ventas", "juanperez@gmail.com", 1234);
+// $empleado->nombre = "Juan";
+// $empleado->apellido = "Perez";
+// $empleado->departamento = "Ventas";
+// $empleado->email = "juanperez@gmail.com";
+// $empleado->codigo = 1234;
+
+echo "<pre>";
+var_dump($empleado);
+echo "</pre>";
