6916d9e6e69434b3851d55c07bbf8988db4b6fc6
Cartago <cartago.relaxingcup@gmail.com>
2023-06-19 19:30:24 +0200

    Probamos parametros y creamos excepcion como prueba

--- introduccion/08.php
+++ introduccion/08.php
@@ -1,6 +1,6 @@
 <?php
-include 'includes/header.php';
 
+include 'includes/header.php';
 
 class Empleado
 {
@@ -21,65 +21,67 @@ class Empleado
 
     public function nombreCompleto(): string
     {
-        return $this->nombre . " " . $this->apellido;
+        return $this->nombre . ' ' . $this->apellido;
     }
 
-    public function getPropsSegunTipo($tipo = null): array
+    public function getPropsSegunTipo(?string $tipo = null): array
     {
+        if ($tipo !== 'public' && $tipo !== 'protected' && $tipo !== 'private' && $tipo !== null) {
+            throw new Exception("Tipo de parametro invalido");
+        }
+
         $properties = get_object_vars($this);
+
+
         $kindProperties = [];
 
-        foreach($properties as $key => $value) {
-            if($tipo === null) {
+        foreach ($properties as $key => $value) {
+            if ($tipo === null) {
                 $kindProperties[$key] = $value;
                 continue;
             }
             $reflectionProperty = new ReflectionProperty($this, $key);
 
-            if($tipo === "public" && $reflectionProperty->isPublic()) {
-              
-                    $kindProperties[$key] = $value;
-        
-            } else if($tipo === "private" && $reflectionProperty->isPrivate()) {
-            
-                    $kindProperties[$key] = $value;
-          
-            }
-            else if ($tipo === "protected" && $reflectionProperty->isProtected()) {
-                
-                    $kindProperties[$key] = $value;
-                
+            if ($tipo === 'public' && $reflectionProperty->isPublic()) {
+                $kindProperties[$key] = $value;
+            } elseif ($tipo === 'private' && $reflectionProperty->isPrivate()) {
+                $kindProperties[$key] = $value;
+            } elseif ($tipo === 'protected' && $reflectionProperty->isProtected()) {
+                $kindProperties[$key] = $value;
             }
         }
+
         return $kindProperties;
     }
 }
 
 $juan = new Empleado(
-    "Juan",
-    "Perez",
-    "Ventas",
-    "juanperez@gmail.com",
+    'Juan',
+    'Perez',
+    'Ventas',
+    'juanperez@gmail.com',
     1234
 );
 
-
-echo "Esto es predefinidio <hr><pre>";
+echo 'Esto es predefinidio <hr><pre>';
 print_r($juan->getPropsSegunTipo());
-echo "</pre>";
+echo '</pre>';
 
-echo "Esto es publico <hr><pre>";
-print_r($juan->getPropsSegunTipo("public"));
-echo "</pre>";
+echo 'Esto es publico <hr><pre>';
+print_r($juan->getPropsSegunTipo('public'));
+echo '</pre>';
 
-echo "Esto es protegido <hr><pre>";
-print_r($juan->getPropsSegunTipo("protected"));
-echo "</pre>";
+echo 'Esto es protegido <hr><pre>';
+print_r($juan->getPropsSegunTipo('protected'));
+echo '</pre>';
 
-echo "Esto es privado <hr><pre>";
-print_r($juan->getPropsSegunTipo("private"));
-echo "</pre>";
+echo 'Esto es privado <hr><pre>';
+print_r($juan->getPropsSegunTipo('private'));
+echo '</pre>';
 
+// echo 'Esto es deberia dar error <hr><pre>';
+// print_r($juan->getPropsSegunTipo('juanito'));
+// echo '</pre>';
 
 // echo "<pre>";
 // var_dump($juan);
